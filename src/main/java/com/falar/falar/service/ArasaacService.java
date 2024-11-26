7453bafa1b1fde2b692d7e924f341595ef21b701
Nave-git <bianchiifpe@gmail.com>
2024-11-26 15:28:27 -0300

    Colocando validação no service de Arasaac

--- src/main/java/com/falar/falar/service/ArasaacService.java
+++ src/main/java/com/falar/falar/service/ArasaacService.java
@@ -1,6 +1,7 @@
 package com.falar.falar.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -14,6 +15,10 @@ public class ArasaacService {
 
     public byte[] fetchPictogramImage(String id, String keyword) {
         String url = String.format("https://static.arasaac.org/pictograms/%s/%s.png", id, keyword);
-        return restTemplate.getForObject(url, byte[].class);
+        try {
+            return restTemplate.getForObject(url, byte[].class);
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new RuntimeException("Pictograma não encontrado para o ID: " + id + " e palavra-chave: " + keyword);
+        }
     }
 }
